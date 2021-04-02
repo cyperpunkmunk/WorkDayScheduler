a5a38ca2e587886dc3695c4f39ff768c8dd59d68
Alien <kendgibsonjr@gmail.com>
2021-04-01 22:33:38 -0500

    added file

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var titleTime = $('#currentDay')
 var currentDay  = moment().format(" dddd, MMM Do YY")
 $(titleTime).text(currentDay) 
 var schedule = $('.container')
-
+var timeBox = $(".input-group mb-3")
 // every second the current time is returned
 setInterval(function(){
     var currentTime = moment().format('LTS')
      return currentTime
 },1000)
 
-var button = $('button')
 
-button.on('click' , function(){
-    console.log(this.parent.id)
-})
+
+function handleFormSubmit(event){
+    event.preventDefault()
+    var spike = event.target
+    
+    
+    console.log(spike.parent())
+}
+
+$('button').on('click' , handleFormSubmit)
